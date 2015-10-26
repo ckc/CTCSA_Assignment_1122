8dedfe7b7fef3a857d2b38e6a44c66a87c23d6df
ckc <mail@ckc.im>
2015-10-26 22:18:50 +0800

    initial

---
+++ src/AJCS/PrimaryMember.java
@@ -0,0 +1,15 @@
+package AJCS;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ckc
+ */
+public class PrimaryMember {
+    
+}
