1d4fe46ce0a252b973b84ab94482940148a93d6b
ckc <ckc@2u.hk>
2015-11-22 03:37:04 +0800

    testing for undo

---
+++ src/Xmem/Action.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Xmem;
+
+/**
+ *
+ * @author ckc
+ */
+
+public class Action {
+	private int option;
+	//private Shape shape;
+	//private int index;
+	
+	public Action(int option/*, Shape shape, int index*/) {
+		this.option = option;
+		/*this.shape = shape;
+		this.index = index;*/
+	}
+	
+	public int getOption() { return option; }
+	/*public Shape getShape() { return shape; }
+	public int getIndex() { return index; }*/
+}
+
